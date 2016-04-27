72b0a17fc6f7183cfdb2bece26f52ad2980439ca
Christian Wöhle <christian@puppet.ifam.uni-kiel.de>
2016-04-27 16:17:07 +0200

    fdr module for fisher.sh

---
+++ fdr.R
@@ -0,0 +1,14 @@
+args=(commandArgs(TRUE))
+  for(i in 1:length(args)){
+      eval(parse(text=args[[i]]));
+    }
+
+pval=pval
+fdr=p.adjust(pval, method = 'fdr')
+
+
+write.table(fdr, file = "fdr.out", append = FALSE, quote = FALSE, sep = " ",
+eol = "\n", na = "NA", dec = ".", row.names = FALSE,
+col.names = FALSE, qmethod = c("escape", "double"),
+fileEncoding = "")
+
---
+++ fisher.R
@@ -0,0 +1,21 @@
+args=(commandArgs(TRUE))
+  for(i in 1:length(args)){
+      eval(parse(text=args[[i]]));
+    }
+
+
+
+data=cbind(oxi,ctl);
+data
+
+
+res=fisher.test(data)
+res
+
+pval=res$p.val
+pval
+
+write.table(pval, file = "pval.temp", append = FALSE, quote = FALSE, sep = " ",
+eol = "\n", na = "NA", dec = ".", row.names = FALSE,
+col.names = FALSE, qmethod = c("escape", "double"),
+fileEncoding = "")
