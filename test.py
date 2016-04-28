2df219e33119eed13543062b3b56ae87388fa7df
Christian Wöhle <christian@ukifam-c156.local>
2016-04-28 13:13:13 +0200

    Python python python

--- test.py
+++ test.py
@@ -257,3 +257,55 @@ test_email(pattern)
 
 
 #####Stopped here: http://www.learnpython.org/en/Functions
+
+##functions
+
+print "\n###Functions\n"
+
+# Modify this function to return a list of strings as defined above
+def list_benefits():
+    return ("More organized code","More readable code","Easier code reuse","Allowing programmers to share and connect code together")
+
+# Modify this function to concatenate to each benefit - " is a benefit of functions!"
+def build_sentence(benefit):
+    return benefit + " is a benefit of functions!"
+
+def name_the_benefits_of_functions():
+    list_of_benefits = list_benefits()
+    for benefit in list_of_benefits:
+        print build_sentence(benefit)
+
+name_the_benefits_of_functions()
+
+####class
+
+print "\n###Class\n"
+
+
+# define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+# your code goes here
+
+car1=Vehicle()
+car1.color="red"
+car1.name="Fer"
+car1.value=60000
+car1.kind="convertible"
+
+car2=Vehicle()
+car2.color="blue"
+car2.name="Jump"
+car2.value=10000
+car2.kind="van"
+
+# test code
+print car1.description()
+print car2.description()
+
