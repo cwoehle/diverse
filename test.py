e2787b56a2412baa7737431b1be7439879011165
Christian Wöhle <christian@puppet.ifam.uni-kiel.de>
2016-04-28 09:39:08 +0200

    Further python

--- test.py
+++ test.py
@@ -4,3 +4,18 @@ x = 1
 if x == 1:
     # indented four spaces
     print "x is 1."
+
+print "x is 1."
+
+# change this code
+mystring = "hello"
+myfloat = float(10)
+myint = 20
+
+# testing code
+if mystring == "hello":
+    print "String: %s" % mystring
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print "Float: %d" % myfloat
+if isinstance(myint, int) and myint == 20:
+    print "Integer: %d" % myint
